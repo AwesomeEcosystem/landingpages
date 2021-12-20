62f7559500e02943b85b495e5455bfb60aa4e05e
equaliser0 <accounts@equaliser.minimalcity.de>
2021-12-20 23:33:39 +0100

    Init Landingpage

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <hero />
+    <features id="about" />
+    <product id="product" />
+    <partners id="partner" />
+    <call-to-action />
+  </div>
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import Product from '@/components/Product'
+import Partners from '@/components/Partners'
+import CallToAction from '@/components/CallToAction'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    hero: Hero,
+    features: Features,
+    product: Product,
+    partners: Partners,
+    'call-to-action': CallToAction
+  }
+}
+</script>
