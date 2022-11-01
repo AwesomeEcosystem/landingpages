ace9bfa766765e21437c075f034729a1b0beda6a
equaliser0 <accounts@equaliser.minimalcity.de>
2022-11-01 12:34:16 +0100

    rename

--- pages/eco.vue
+++ pages/eco.vue
@@ -4,8 +4,7 @@
       <div class="container px-3 mx-auto flex flex-col items-center w-full pt-12">
         <!--Left Col-->
         <div class="w-full text-center flex justify-center">
-          https://www.freepik.com/free-vector/digital-smart-city-concept-big-data-transmission-processing-data-center-warehouse_3629619.htm#&position=5&from_view=collections
-          <img src="~/assets/logos/bend_logo.png"/>
+          <img src="~/assets/logos/one_logo.png"/>
         </div>
         <!--Right Col-->
         <div class="flex flex-col w-full justify-center items-center text-center md:text-left">
@@ -13,61 +12,40 @@
             Eco
           </h1>
           <p class="leading-normal text-2xl mb-8 font-thin">
-            Reserved Bridgechain
+            The All-in-One ecosis Network Wallet
           </p>
 
-          <button class="mx-auto lg:mx-0 hover:underline hot text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg flex items-center" @click="$router.push('/launch')">
-            Join Sale!
-          </button>
+          <a class="mx-auto lg:mx-0 hover:underline hot text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg flex items-center" href="https://app.ecosis.network" target="_blank">
+            Open App
+          </a>
         </div>
       </div>
     </div>
 
     <Wave align="top" color="#fafafa"/>
-    <section>
-      <div class="flex">
-        <div class="">
-          https://www.freepik.com/free-vector/isometric-networking-connection-internet-network-topology-concept-server-room_3629630.htm
-        </div>
-        <div class="">
-          <h1>What is Eco</h1>
-          <p>Eco is a Bridgechain enabling the digital economical infrastructure.</p>
-          <p>A Blockchain how it should!</p>
-          <p>This Bridgechain is the base for the ecosis platform at the very bottom. </p>
-          <p>Could run a smartphone.</p>
-          <p>Fast Transactions.</p>
-          <p>Best Developer Adoption due it's developed with the worlds most wellknown programming language Typescript.</p>
-          <p>The Blockchain which enables cross-chain decentralized exchanges!</p>
-        </div>
-        <div class="w-full">
-          <h1>The most efficient and democratic Blockchain on the world!</h1>
-          <p>Written in Typescript</p>
-          <p>Bulletproof by dozens of hackathons</p>
-          <p>Easiest to use - say good bye to crypto handling</p>
-        </div>
+    <div class="flex justify-center w-full text-center px-4 bg-gray-100 text-gray-800 pt-12">
+      <div class="w-full md:w-1/2">
+
       </div>
-    </section>
+    </div>
     <Wave align="bottom" color="#fafafa"/>
 
     <div class="py-32">
-      <p></p>
-      <h1>Stable Economics</h1>
-      <div class="">
-        <p>But a Tax System in a way how it sould, thanks to Custom Transaction Types.</p>
-        <p>This Bridgechain is not only backed by collaterals in Nomics Vaults across multiple chains, it bridges Tokens in the most secure way!</p>
-      </div>
-      <div class="">
-        <h2></h2>
-      </div>
-    </div>
+      <div class="container px-3 mx-auto flex flex-col items-center w-full pt-12">
+        <!--Left Col-->
+        <div class="w-full text-center flex justify-center">
+          <img src="~/assets/logos/one_logo_light.png"/>
+        </div>
+        <p class="leading-normal text-2xl mb-8 font-thin">
+          The All-in-One ecosis Network Wallet
+        </p>
+        <!--Right Col-->
+        <div class="flex flex-col w-full justify-center items-center text-center md:text-left">
 
-    <div class="py-32">
-      https://www.freepik.com/free-vector/awesome-isometric-banner-digital-technology-isometric-abstract-icon-big-data-processing_4102891.htm#&position=0&from_view=collections
-      <p>Delegate Prof of Stake</p>
-      <h1>Delegates</h1>
-      <div class="">
-        <p>Best Blockchain Consensus on the world so far!</p>
-        <p>Bringing democracy to everyone!</p>
+          <a class="mx-auto lg:mx-0 hover:underline hot text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg flex items-center" href="https://app.ecosis.network" target="_blank">
+            Open App
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -83,11 +61,10 @@ import FeatureRight from '@/components/FeatureRight'
 import LineChart from "@/components/Charts/LineChart"
 import DoughnutChart from "@/components/Charts/DoughnutChart"
 
-import Presale from '@/assets/svg/percent-solid.svg'
+import Bank from '@/assets/svg/piggy-bank-solid.svg'
 import Team from '@/assets/svg/users-solid.svg'
-import Marketing from '@/assets/svg/arrows-to-eye-solid.svg'
-import Mainsale from '@/assets/svg/cart-arrow-down-solid.svg'
-import Liquidity from '@/assets/svg/circle-dollar-to-slot-solid.svg'
+import Partners from '@/assets/svg/hands-helping-solid.svg'
+import Market from '@/assets/svg/globe-solid.svg'
 
 export default {
   data() {
@@ -102,18 +79,27 @@ export default {
         ],
         datasets: [
           {
-            label: "Deflation",
+            label: "Bend",
             borderColor: "#FC2525",
             pointBackgroundColor: "white",
             borderWidth: 1,
             pointBorderColor: "white",
             backgroundColor: this.gradient,
-            data: [2, 5, 15, 35, 65],
+            data: [2, 10, 15, 35, 65],
+          },
+          {
+            label: "Partner Tokens",
+            borderColor: "#05CBE1",
+            pointBackgroundColor: "white",
+            pointBorderColor: "white",
+            borderWidth: 1,
+            backgroundColor: this.gradient2,
+            data: [30, 55, 4, 50, 75],
           },
         ],
       },
       doughnutData: {
-        labels: ['Presale', 'Mainsale', 'Team', 'Mainsaleing', 'Liquidity'],
+        labels: ['Presale', 'eLabs', 'Genesis Partner Distribution', 'Market'],
         datasets: [
           {
             label: "Bend",
@@ -121,7 +107,7 @@ export default {
             pointBackgroundColor: "white",
             borderWidth: 1,
             backgroundColor: ['#5D675B', '#E5B25D', '#4357AD', '#EF5B5B'],
-            data: [20, 40, 10, 10, 20]
+            data: [20, 20, 20, 40]
           }
         ]
       },
@@ -137,11 +123,10 @@ export default {
     DoughnutChart,
     FeatureLeft,
     FeatureRight,
-    Presale,
+    Bank,
     Team,
-    Marketing,
-    Mainsale,
-    Liquidity
+    Partners,
+    Market
   }
 }
 </script>
