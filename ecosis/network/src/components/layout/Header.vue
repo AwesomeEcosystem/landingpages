fb84c23f0cb6c1bc64513922af6d634534730796
equaliser0 <accounts@equaliser.minimalcity.de>
2023-05-11 08:55:57 +0200

    fix header by remove backdrop

--- ecosis/network/src/components/layout/Header.vue
+++ ecosis/network/src/components/layout/Header.vue
@@ -1,44 +1,43 @@
 <template>
-    <header class="sticky top-0 z-50 bg-black/10 transition duration-300 !backdrop-blur-md">
+    <header class="sticky top-0 z-50 bg-black/10 transition duration-300">
         <div class="container">
             <div class="flex items-center justify-between py-5 lg:py-0">
-                <router-link to="/"><img src="/assets/logos/ecosis_v2.png" alt="logo" class="h-10" /></router-link>
-                <div class="flex items-center">
-                    <div @click="toggleMenu()" class="overlay fixed inset-0 z-[51] bg-black/60" :class="{ hidden: !showMenu }"></div>
-                    <div class="menus" :class="{ 'overflow-y-auto ltr:!right-0 rtl:!left-0': showMenu }">
-                        <div class="border-b border-gray/10 ltr:text-right rtl:text-left lg:hidden">
-                            <button @click="toggleMenu()" type="button" class="p-4">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="h-6 w-6 text-black dark:text-white"
-                                >
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
+              <router-link to="/"><img src="/assets/logos/ecosis_v2.png" alt="logo" class="h-10" /></router-link>
+              <div class="flex items-center">
+                  <div @click="toggleMenu()" class="overlay fixed inset-0 z-[51] bg-black/60" :class="{ hidden: !showMenu }"></div>
+                  <div class="menus" :class="{ 'overflow-y-auto ltr:!right-0 rtl:!left-0': showMenu }">
+                      <div class="border-b border-gray/10 ltr:text-right rtl:text-left lg:hidden">
+                          <button @click="toggleMenu()" type="button" class="p-4">
+                              <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke-width="1.5"
+                                  stroke="currentColor"
+                                  class="h-6 w-6 text-black dark:text-white"
+                              >
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                              </svg>
+                          </button>
+                      </div>
 
-                        <ul @click="showMenu = false">
-                            <!-- <li><router-link to="/#about" :class="{ active: $route.name === '/' }"> About </router-link></li> -->
-                            <li>
-                                <router-link to="/#about" :class="{ active: $route.name === '#about' }">About</router-link>
-                            </li>
-                            <li>
-                                <router-link to="/#guide" :class="{ active: $route.name === '#guide' }">Guide</router-link>
-                            </li>
-                            <li>
-                                <router-link to="/#team" :class="{ active: $route.name === '#team' }">Team</router-link>
-                            </li>
+                      <ul @click="showMenu = false">
+                          <!-- <li><router-link to="/#about" :class="{ active: $route.name === '/' }"> About </router-link></li> -->
+                          <li>
+                              <router-link to="/#about" :class="{ active: $route.name === '#about' }">About</router-link>
+                          </li>
+                          <li>
+                              <router-link to="/#guide" :class="{ active: $route.name === '#guide' }">Guide</router-link>
+                          </li>
+                          <li>
+                              <router-link to="/#team" :class="{ active: $route.name === '#team' }">Team</router-link>
+                          </li>
 
-                            <div class="m-2">
-                              <a href="https://airtable.com/shruP2nuVOBvsktsf" target="_blank" class="btn mx-auto mt-2 block w-fit bg-white lg:mx-0 lg:rtl:ml-auto px-4">Get Early Access</a>
-                            </div>
-                        </ul>
+                          <div class="m-2">
+                            <a href="https://airtable.com/shruP2nuVOBvsktsf" target="_blank" class="btn mx-auto mt-2 block w-fit bg-white lg:mx-0 lg:rtl:ml-auto px-4">Get Early Access</a>
+                          </div>
+                      </ul>
                     </div>
-
                     <button type="button" class="flex h-10 w-10 items-center justify-center rounded-full bg-primary lg:hidden" @click="toggleMenu()">
                         <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-white">
                             <path
